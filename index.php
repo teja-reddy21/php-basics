8c0a4db5a6a33235a2fff8fabce5b670441c58e4
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-06-27 21:10:20 +0530

    arrays

--- index.php
+++ index.php
@@ -14,42 +14,4 @@ $has_kids= true;
 // $x="5" + "4";
 // var_dump($x);
 
-//simplet array
-$number=[1,45,67,31];
-// print_r($number);
-$fruits=array('apple','orange','pear');
-// echo $fruits[0];
-
-//associative array
-
-$colors= [
-  1=>'red',
-  4=>'blue',
-  6=>'green'
-];
-// echo $colors[4];
-
-$hex=[
-  'red'=>'#f00',
-  'blue'=>'#0f0',
-  'green'=>'#00f'
-];
-// echo $hex['blue'];
-
-$person=[
-  'first_name'=>'teju',
-  'last_name'=>'nimmanapalli'
-];
-// echo $person['first_name'];
-$peple=[
-  [
-  'first_name'=>'jyo',
-  'last_name'=>'sanapu'
-],
-[
-  'first_name'=>'lari',
-  'last_name'=>'batala'
-]
-];
-echo $peple[0]['last_name'];
 ?>
