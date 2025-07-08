8b89780487296e599b3f9c77ae40e16fc10cae85
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-08 20:40:24 +0530

    php switch case

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+
+ /* switch */
+  $favcolor='red';
+
+  switch($favcolor){
+    case 'red':
+      // echo 'your favcolor is red';
+      break;
+      case 'blue':
+        // echo 'your favcolor is blue';
+        break;
+        case 'green':
+          // echo 'your favcolor is green';
+          break;
+          default:
+          // echo 'your favcolor is not red , green , or blue';
+  }
+?>
