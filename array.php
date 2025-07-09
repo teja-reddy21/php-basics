03fde19c38d1b368e6bd70a805ffb78fd230cc62
teja-reddy21 <tejanimmanapalliteja@gmail.com>
2025-07-09 21:19:18 +0530

    php arrays

---
+++ array.php
@@ -0,0 +1,42 @@
+<?php
+
+//simplet array
+$number=[1,45,67,31];
+// print_r($number);
+$fruits=array('apple','orange','pear');
+// echo $fruits[0];
+
+//associative array
+
+$colors= [
+  1=>'red',
+  4=>'blue',
+  6=>'green'
+];
+// echo $colors[4];
+
+$hex=[
+  'red'=>'#f00',
+  'blue'=>'#0f0',
+  'green'=>'#00f'
+];
+// echo $hex['blue'];
+
+$person=[
+  'first_name'=>'teju',
+  'last_name'=>'nimmanapalli',
+  'email_name'=>"teju@gmail.com"
+];
+// echo $person['first_name'];
+$peple=[
+  [
+  'first_name'=>'jyo',
+  'last_name'=>'sanapu'
+],
+[
+  'first_name'=>'lari',
+  'last_name'=>'batala'
+]
+];
+// echo $peple[0]['last_name'];
+?>
